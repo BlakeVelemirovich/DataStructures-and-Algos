773fbf957bfd9afe62b86e83cc086d020bcf4d45
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-09-18 17:56:48 -0300

    Hashmap generation, almost finished sliding window algo

--- Sliding Window Algos/Permutation in a String (hard)/Permutation In a String.js
+++ Sliding Window Algos/Permutation in a String (hard)/Permutation In a String.js
@@ -1,8 +1,32 @@
 function permutationChecker(arr, pattern) {
     let patternMap = {};
     let windowStart = 0;
+    let matched = 0;
 
-    
+    //generating hashmap to be used for checking pattern matches
+    for (i = 0; i < pattern.length; i++) {
+        const char = pattern[i];
+        if (!(char in patternMap)) {
+            patternMap[char] = 0;
+        }
+        patternMap[char]++;
+    }
+
+    //sliding window algo, check each char against hashmap. If all value in hashmap is zero then it's a match
+    for (windowEnd = 0; windowEnd < arr.length; windowEnd) {
+        const rightChar = arr[windowEnd];
+        //if there is a match decrement hashmap char and add to matched counter
+        if (rightChar in patternMap) {
+            patternMap[rightChar]--;
+            if (patternMap[rightChar] === 0) {
+                matched++;
+            }
+        }
+        if (Object.keys(patternMap).length === matched) {
+            return true;
+        }
+
+    }
 }
 
 
