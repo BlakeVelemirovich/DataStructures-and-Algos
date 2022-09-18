0e943d6c2f9da7e5627d9c008bb9dabea6911f56
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-09-18 17:09:13 -0300

    Starting files

---
+++ Sliding Window Algos/Permutation in a String (hard)/Permutation In a String.js
@@ -0,0 +1,9 @@
+function permutationChecker(arr, pattern) {
+    let patternMap = {};
+    let windowStart = 0;
+
+    
+}
+
+
+console.log(`Permutation exist: ${permutationChecker('oidbcaf', 'abc')}`);
