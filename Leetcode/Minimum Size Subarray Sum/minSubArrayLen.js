1a2fc59f25e28010511ee3fdf49e3e25df3097ea
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-10-22 12:38:09 -0300

    Algo passes Leetcode

--- Leetcode/Minimum Size Subarray Sum/minSubArrayLen.js
+++ Leetcode/Minimum Size Subarray Sum/minSubArrayLen.js
@@ -1,12 +1,15 @@
 var minSubArrayLen = function(target, nums) {
     let currentMinimumLength = Infinity;
-    let windowStart = 1;
+    let windowStart = 0;
     let sum = 0;
 
+    //Iterate through entire input array
     for (windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+        //Sliding window algorithm, adding right most char to sum
         let rightChar = nums[windowEnd]; 
         sum += rightChar;
 
+        //If we have met target, track smallest subarray that met target and then shrink window
         while (sum >= target) {
             currentMinimumLength = Math.min(currentMinimumLength, windowEnd - windowStart + 1);
             sum -= nums[windowStart];
@@ -15,6 +18,11 @@ var minSubArrayLen = function(target, nums) {
 
     }
 
+    if (currentMinimumLength === Infinity) {
+        return 0;
+    }
     return currentMinimumLength;
 
 };
+
+console.log(minSubArrayLen(15, [1,2,3,4,5]));
