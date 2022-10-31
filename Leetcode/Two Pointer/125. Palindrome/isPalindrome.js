470bd5d2ff7b44d8a24527502c1755c41f688f44
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-10-31 15:55:42 -0300

    Finished algo passes all test cases and submission approved

--- Leetcode/Two Pointer/125. Palindrome/isPalindrome.js
+++ Leetcode/Two Pointer/125. Palindrome/isPalindrome.js
@@ -3,14 +3,14 @@ var isPalindrome = function(s) {
     let rightPointer = s.length - 1;
 
     while (leftPointer < rightPointer) {
-        while (leftPointer < rightPointer && !(alphaNumChecker(s[leftPointer]))) {
+        while (leftPointer < rightPointer && !(alphaNumChecker(s.charCodeAt(leftPointer)))) {
             leftPointer++;
         }
-        while (rightPointer > leftPointer && !(alphaNumChecker(s[rightPointer]))) {
+        while (rightPointer > leftPointer && !(alphaNumChecker(s.charCodeAt(rightPointer)))) {
             rightPointer--;
         }
 
-        if (toLowerCase(s[leftPointer]) != toLowerCase(s[rightPointer])) {
+        if (toLowerCase(s.charCodeAt(leftPointer)) !== toLowerCase(s.charCodeAt(rightPointer))) {
             return false;
         }
         leftPointer++;
@@ -18,24 +18,22 @@ var isPalindrome = function(s) {
     }
     
     return true;
-}
+};
 
 var alphaNumChecker = function(char) {
-    if (!(char > 47 && char < 58) && 
-        !(char > 64 && char < 91) && 
-        !(char > 96 && char < 123)) { 
+    if (!(char > 47 && char < 58) && // numeric (0-9)
+        !(char > 64 && char < 91) && // upper alpha (A-Z)
+        !(char > 96 && char < 123)) { // lower alpha (a-z)
       return false;
     }
     return true;
 }
 
-var toLowerCase = function(code) {
-    if (code >= 65 && code <= 90) {
-        return code + 32    
+var toLowerCase = function(char) {
+    if (char >= 65 && char <= 90) {
+        return char + 32    
     }
     else {
-        return code
+        return char
     }
 }
-
-console.log("race a car");
