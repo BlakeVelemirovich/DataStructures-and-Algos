2df053110bdc3d3657f9980431fe3d51dce94c0f
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-06 13:28:36 -0400

    Passes all test cases and submitted

---
+++ Leetcode/Two Pointer/27. Remove Elements/removeElement.js
@@ -0,0 +1,10 @@
+var removeElement = function(nums, val) {
+    let leftPointer = 0;
+
+    for (rightPointer = 0; rightPointer < nums.length; rightPointer++) {
+        if (nums[rightPointer] != val) {
+            nums[leftPointer++] = nums[rightPointer];
+        } 
+    }
+    return leftPointer;
+};
