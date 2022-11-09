4dc38a3eba33682ad92d6fd366dceda6a223e31b
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-08 22:39:08 -0400

    Submitted and accepted

--- Leetcode/Two Pointer/88. Merge Sorted Array/merge.js
+++ Leetcode/Two Pointer/88. Merge Sorted Array/merge.js
@@ -1,3 +1,21 @@
 var merge = function(nums1, m, nums2, n) {
-    
+    let replacePointer = m + n - 1;
+
+    while (m > 0 && n > 0) {
+        if (nums1[m - 1] > nums1[n - 1]) {
+            nums1[replacePointer] = nums1[m - 1];
+            m--;
+        }
+        else {
+            nums1[replacePointer] = nums2[n - 1];
+            n--;
+        }
+        replacePointer--;
+    }
+
+    while (n > 0) {
+        nums1[replacePointer] = nums2[n - 1];
+        n--;
+        replacePointer--;
+    }
 };
