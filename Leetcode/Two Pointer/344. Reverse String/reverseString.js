45e4b4bde158e286758bf9a900bf0c6af6600a2f
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-10 13:26:52 -0400

    Submitted and accepted

--- Leetcode/Two Pointer/344. Reverse String/reverseString.js
+++ Leetcode/Two Pointer/344. Reverse String/reverseString.js
@@ -1,3 +1,11 @@
 var reverseString = function(s) {
+    let leftPointer = 0;
+    let rightPointer = s.length - 1;
+    let temp;
     
+    while (leftPointer < rightPointer) {
+        temp = s[leftPointer];
+        s[leftPointer++] = s[rightPointer];
+        s[rightPointer--] = temp;
+    }
 };
