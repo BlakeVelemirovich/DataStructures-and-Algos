9c6d96c4d9bb7dbc86c7a95b43af54719a7a0d63
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-11 10:58:03 -0400

    Finished and submitted

--- Leetcode/Two Pointer/167. Two Sum II - Input Array Is Sorted/twoSum.js
+++ Leetcode/Two Pointer/167. Two Sum II - Input Array Is Sorted/twoSum.js
@@ -1,3 +1,16 @@
 var twoSum = function(numbers, target) {
-    
+    let rightPointer = numbers.length - 1;
+    let leftPointer = 0;
+
+    while (leftPointer < rightPointer) {
+        if (numbers[leftPointer] + numbers[rightPointer] === target) {
+            return [leftPointer + 1, rightPointer + 1];
+        }
+        else if (numbers[leftPointer] + numbers[rightPointer] > target) {
+            rightPointer--;
+        }
+        else {
+            leftPointer++;
+        }
+    }
 };
