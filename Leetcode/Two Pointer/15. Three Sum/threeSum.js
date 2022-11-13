6eea7ca556941c541dc131f0b4ec3a2db54cfe2f
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-13 15:29:41 -0400

    Logic is sound but code is wrong somewhere

--- Leetcode/Two Pointer/15. Three Sum/threeSum.js
+++ Leetcode/Two Pointer/15. Three Sum/threeSum.js
@@ -1,3 +1,30 @@
 var threeSum = function(nums) {
-    
+    nums.sort();
+
+    let result = [];
+    let target = 0;
+
+    for (i = 0; i < nums.length - 2; i++) {
+        let leftPointer = i + 1;
+        let rightPointer = nums.length - 1;
+
+        if (nums[i] > target) break;
+        if (i > 0 && nums[i] === nums[leftPointer]) continue;
+
+        while (leftPointer < rightPointer) {
+            let sum = nums[i] + nums[leftPointer] + nums[rightPointer];
+
+            if (sum === target) {
+                result.push(nums[i], nums[leftPointer], nums[rightPointer]);
+                continue;
+            }
+            else if (sum > target) {
+                leftPointer++;
+            }
+            else {
+                rightPointer--;
+            }
+        }
+
+    }
 };
