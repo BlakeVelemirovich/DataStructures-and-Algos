f342585d62d28eef27cc69cc31e349330d5b48b0
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-13 21:30:05 -0400

    Finished and submitted

--- Leetcode/Two Pointer/11. Container With Most Water/maxArea.js
+++ Leetcode/Two Pointer/11. Container With Most Water/maxArea.js
@@ -1,3 +1,30 @@
 var maxArea = function(height) {
-    
+    let rightPointer = height.length - 1;
+    let leftPointer = 0;
+    let result = 0;
+
+    while (leftPointer < rightPointer) {
+        if (height[leftPointer] < height[rightPointer]) {
+            lineHeight = height[leftPointer];
+        }
+        else {
+            lineHeight = height[rightPointer];
+        }
+        result = Math.max(result, width * lineHeight);
+
+        if (height[leftPointer] < height[rightPointer]) {
+            leftPointer++;
+        }
+        else if (height[leftPointer] > height[rightPointer]) {
+            rightPointer--;
+        }
+        else if (height[leftPointer + 1] > height[rightPointer + 1]) {
+            leftPointer++;
+        }
+        else {
+            rightPointer--;
+        }
+        
+    }
+    return result;
 };
