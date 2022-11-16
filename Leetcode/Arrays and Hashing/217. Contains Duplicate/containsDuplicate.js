85db5d938b921e478456266a674e74459605e0e2
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-16 17:04:22 -0400

    Tested and submitted hashmap solution

--- Leetcode/Arrays and Hashing/217. Contains Duplicate/containsDuplicate.js
+++ Leetcode/Arrays and Hashing/217. Contains Duplicate/containsDuplicate.js
@@ -1,3 +1,15 @@
 var containsDuplicate = function(nums) {
-    
+    var containsDuplicate = function(nums) {
+        let hashMap = {};
+        
+        for (i = 0; i < nums.length; i++) {
+            if (hashMap[nums[i]] === undefined) {
+                hashMap[nums[i]] = i;
+            } 
+            else {
+                return true;
+            }
+        }
+        return false;
+    };
 };
