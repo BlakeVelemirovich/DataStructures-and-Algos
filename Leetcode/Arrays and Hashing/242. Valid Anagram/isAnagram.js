62bd376706977368faa63f06f02065469f5bfb6e
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-16 22:30:01 -0400

    Bed time, got a hashmap generated with t array occurences

--- Leetcode/Arrays and Hashing/242. Valid Anagram/isAnagram.js
+++ Leetcode/Arrays and Hashing/242. Valid Anagram/isAnagram.js
@@ -1,3 +1,18 @@
 var isAnagram = function(s, t) {
-    
+    let hashMap = {};
+    let matched = 0;
+
+    for (i = 0; i < t.length; i++) {
+        if (!(t[i] in hashMap)) {
+            hashMap[t[i]] = 0;
+        }
+        hashMap[t[i]]++;
+    }
+
+    for (i = 0; i < s.length; i++) {
+        if (s[i] in hashMap) {
+            hashMap
+        }
+    }
+
 };
