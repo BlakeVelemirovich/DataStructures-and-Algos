d3ae519580b7c680a46ccad4fd240ceb92d94392
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-18 15:21:32 -0400

    Completed and submitted.

--- Leetcode/Arrays and Hashing/49. Group Anagrams/groupAnagrams.js
+++ Leetcode/Arrays and Hashing/49. Group Anagrams/groupAnagrams.js
@@ -1,3 +1,11 @@
 var groupAnagrams = function(strs) {
-    
+    let hashMap = {};
+
+    strs.forEach(word => {
+        let sortedWord = word.split("").sort().join("");
+        
+        hashMap[sortedWord] ? hashMap[sortedWord].push(word) : hashMap[sortedWord] = [word];
+    });
+
+    return Object.values(hashMap);
 };
