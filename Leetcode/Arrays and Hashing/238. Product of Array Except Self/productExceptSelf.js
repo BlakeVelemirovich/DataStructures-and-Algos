bd82a0b94e332a8c561d327099e4c1b18f2da79f
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-11-20 13:45:09 -0400

    Finished and Submitted

--- Leetcode/Arrays and Hashing/238. Product of Array Except Self/productExceptSelf.js
+++ Leetcode/Arrays and Hashing/238. Product of Array Except Self/productExceptSelf.js
@@ -1,3 +1,17 @@
 var productExceptSelf = function(nums) {
-    
+    let rightMultiplication = 1;
+    let leftMultiplication = 1;
+    let result = [];
+
+    for (i = nums.length - 1; i > nums.length; i--) {
+        result[i] = leftMultiplication;
+        leftMultiplication *= nums[i];
+    }
+
+    for (i = 0; nums.length; i++) {
+        result[i] = leftMultiplication * result[i];
+        leftMultiplication *= nums[i];
+    }
+
+    return result;
 };
