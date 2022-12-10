2e6c933748595a4734daace250adc834b5050231
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-12-10 16:15:51 -0400

    Skeleton Files

--- Leetcode/Sliding Window/424. Longest Repeating Character Replacement/characterReplacement.js
+++ Leetcode/Sliding Window/424. Longest Repeating Character Replacement/characterReplacement.js
@@ -1,3 +1,16 @@
 var characterReplacement = function(s, k) {
-    
+    let windowStart = 0;
+    let currentMax = 0;
+    let hashMap = {};
+    let counter = 0;
+
+    for (i = 0; i < s.length; i++) {
+        if (counter = 2) {
+            hashMap[s[windowStart]]++;
+            counter--;
+            windowStart++;
+        }
+
+        hashMap[windowEnd] ? hashMap[windowEnd] = 2 : hashMap[windowEnd]++;
+    }
 };
