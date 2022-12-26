813c9fe7ffb2f70151805a0754058e9b729e7aa5
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-12-25 20:15:31 -0400

    Finished and submitted

--- Leetcode/Binary Search/704. Binary Search/search.js
+++ Leetcode/Binary Search/704. Binary Search/search.js
@@ -1,3 +1,13 @@
 var search = function(nums, target) {
-    
+    let leftPointer = 0;
+    let rightPointer = nums.length - 1;
+
+    while (leftPointer <= rightPointer) {
+        
+        let middlePointer = Math.floor((leftPointer + rightPointer) / 2);
+        if (nums[middlePointer] === target) return middlePointer;
+        else if (target > nums[middlePointer]) leftPointer = middlePointer + 1;
+        else rightPointer = middlePointer - 1;
+    }
+    return - 1;
 };
