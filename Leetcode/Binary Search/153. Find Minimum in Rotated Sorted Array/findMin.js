eff33779340ddb727a20e072ede8208387aa620e
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-12-30 22:05:17 -0400

    Finished and submitted

--- Leetcode/Binary Search/153. Find Minimum in Rotated Sorted Array/findMin.js
+++ Leetcode/Binary Search/153. Find Minimum in Rotated Sorted Array/findMin.js
@@ -1,3 +1,11 @@
 var findMin = function(nums) {
-    
+    let leftPointer = 0;
+    let rightPointer = nums.length - 1;
+    let midPointer;
+
+    while (leftPointer < rightPointer) {
+        midPointer = ~~((leftPointer + rightPointer) / 2);
+        nums[midPointer] > nums[rightPointer] ? leftPointer = midPointer + 1 : rightPointer = midPointer;
+    }
+    return nums[leftPointer];
 };
