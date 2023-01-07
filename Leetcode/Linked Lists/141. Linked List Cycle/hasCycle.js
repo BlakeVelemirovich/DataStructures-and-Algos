06a3a2bd6597ec634a74c9cc148fdb04a0964d0b
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-01-07 17:25:19 -0400

    Finalized and submitted

--- Leetcode/Linked Lists/141. Linked List Cycle/hasCycle.js
+++ Leetcode/Linked Lists/141. Linked List Cycle/hasCycle.js
@@ -1,5 +1,13 @@
 var hasCycle = function(head) {
     
-    
+    let [slow, fast] = [head, head];
 
-};
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (slow === fast) return true;
+    }
+    
+    return false;
+}
