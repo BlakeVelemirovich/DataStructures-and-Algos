eb1810e82da9eedb6e9b356761f4b21eb2ab20d4
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-01-07 14:40:38 -0400

    Finalized and submitted

--- Leetcode/Linked Lists/206. Reverse Linked List/reverseList.js
+++ Leetcode/Linked Lists/206. Reverse Linked List/reverseList.js
@@ -1,3 +1,12 @@
 var reverseList = function(head) {
     
+    let [prev, crnt, nxt] = [null, head, null];
+
+    while (crnt) {
+        nxt = crnt.next;
+        crnt.next = prev;
+        prev = crnt;
+        crnt = nxt;
+    }
+    return prev;
 };
