20149c7d04853d0d951ea9fbe1e51e6f5c3b9dfe
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-01-07 13:44:57 -0400

    Finalized and Submitted

--- Leetcode/Linked Lists/21. Merge Two Sorted Lists/mergeTwoLists.js
+++ Leetcode/Linked Lists/21. Merge Two Sorted Lists/mergeTwoLists.js
@@ -1,3 +1,20 @@
 var mergeTwoLists = function(list1, list2) {
     
+    let merged = {val: -1, next: null},
+        crnt = merged;
+    
+    while (list1 && list2) {
+        if (list1.val > list2.val) {
+            crnt.next = list2;
+            list2 = list2.next;
+        }
+        else {
+            crnt.next = list1;
+            list1 = list1.next;
+        }
+        crnt =crnt.next;
+    }
+
+    crnt.next = list1 || list2;
+    return merged.next;
 };
