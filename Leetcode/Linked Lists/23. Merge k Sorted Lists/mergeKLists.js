3506093e02e9a1ca694906558489440932d278b6
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-01-09 21:43:33 -0400

    Skeleton Files

--- Leetcode/Linked Lists/23. Merge k Sorted Lists/mergeKLists.js
+++ Leetcode/Linked Lists/23. Merge k Sorted Lists/mergeKLists.js
@@ -1,3 +1,38 @@
+var mergeTwoLists = function(firstList, secondList) {
+    
+    let mergedList = new ListNode(0),
+        crnt = mergedList;
+
+    while (firstList && secondList) {
+        if (firstList.val > secondList.val) {
+            crnt.next = secondList;
+            secondList = secondList.next;
+        }
+        else {
+            crnt.next = firstList;
+            firstList = firstList.next;
+        }
+    
+        crnt = crnt.next;
+    }
+
+    crnt.next = firstList || secondList;
+
+    return mergedList.next;
+}
+
 var mergeKLists = function(lists) {
     
+    if (lists.length === 0 ) {
+        return null;
+    }
+
+    while (lists.length > 1) {
+        let firstList = lists.shift();
+        let secondList = lists.shift();
+        let mergedList = mergeTwoLists(firstList, secondList);
+        lists.push(mergedList);
+    }
+
+    return lists[0];
 };
