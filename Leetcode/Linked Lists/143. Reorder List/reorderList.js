2f26e944210e49159c2fcf9b6e609867406573c9
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-01-11 11:45:24 -0400

    Mid point function created to find the mid point of our linked list

--- Leetcode/Linked Lists/143. Reorder List/reorderList.js
+++ Leetcode/Linked Lists/143. Reorder List/reorderList.js
@@ -1,3 +1,29 @@
+var findMid = function(node) {
+
+    let slow = node;
+    let fast = node;
+
+    while(fast !== null) {
+        if (fast.next !== null && fast.next.next !== null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        else {
+            fast = null;
+        }
+    }
+
+    const midPoint = slow.next;
+    slow.next = null;
+    return midPoint;
+}
+
+var reversedMid = function(node) {
+    
+}
+
 var reorderList = function(head) {
     
+    let midPoint = findMid(head);
+    let reversedMid = reverseList(midPoint);
 };
