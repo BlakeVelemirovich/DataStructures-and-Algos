11f116534a1ee9cbb51da177e5755aefcc3fdf2a
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-01-15 15:10:03 -0400

    Adding seperate function for use case

--- Leetcode/Trees/572. Subtree of Another Tree/isSubtree.js
+++ Leetcode/Trees/572. Subtree of Another Tree/isSubtree.js
@@ -1,3 +1,26 @@
+var compareTrees = function(root, subRoot) {
+
+    if (root.val === subRoot.val) {
+
+        let leftNode = compareTrees(root.left, subRoot.left);
+        let rightNode = compareTrees(root.right, subRoot.right);
+
+        if (leftNode && rightNode) return true;
+    }
+}
+
 var isSubtree = function(root, subRoot) {
     
+    if (!root || !subRoot) return false;
+
+    if (root.val === subRoot.val) {
+        let isEqual = compareTrees(root, subRoot);
+    }
+
+    isSubtree(root.left, subRoot);
+    isSubtree(root.right, subRoot);
+
+    if (isEqual) return true;
+    else return false;
+
 };
