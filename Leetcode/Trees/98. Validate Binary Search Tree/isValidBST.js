d94cf3ab78daa3a75c7ec24fc24a5fe710898008
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-01-18 16:57:38 -0400

    Finalized and Submitted

--- Leetcode/Trees/98. Validate Binary Search Tree/isValidBST.js
+++ Leetcode/Trees/98. Validate Binary Search Tree/isValidBST.js
@@ -1,3 +1,17 @@
-var isValidBST = function(root) {
+var levelOrder = function(root) {
     
+    let [que, ans] = [[root], []];
+
+    while (que[0]) {
+        let [qlen, row] = [que.length, []];
+
+        for (i = 0; i < qlen; i++) {
+            let crnt = que.shift();
+            row.push(crnt.val);
+            if (crnt.left) que.push(crnt.left);
+            if (crnt.right) que.push(crnt.right);
+        }
+        ans.push(row);
+    }
+    return ans;
 };
