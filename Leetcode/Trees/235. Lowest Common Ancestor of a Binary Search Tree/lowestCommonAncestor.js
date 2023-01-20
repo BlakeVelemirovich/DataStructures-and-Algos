c43a46c72ac6eb4bca56226b04f2f41e28fdd94f
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-01-19 21:52:11 -0400

    Finished and Submitted

--- Leetcode/Trees/235. Lowest Common Ancestor of a Binary Search Tree/lowestCommonAncestor.js
+++ Leetcode/Trees/235. Lowest Common Ancestor of a Binary Search Tree/lowestCommonAncestor.js
@@ -1,3 +1,10 @@
 var lowestCommonAncestor = function(root, p, q) {
     
+    let crnt = root;
+
+    while (crnt) {
+        if (p < crnt.val && q < crnt.val) crnt.left;
+        else if (p > crnt.val && q > crnt.val) crnt.right;
+        else return crnt;
+    }
 };
